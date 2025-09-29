d158fc3e17c001026bf46013c2400d3268a7b735
s2imonovic <s2imonovic@gmail.com>
2025-09-29 22:33:25 +0200

    refactor: revert changes in the helper interfaces

--- contracts/Errors.sol
+++ contracts/Errors.sol
@@ -4,5 +4,6 @@ pragma solidity 0.8.26;
 /// @title INotSupportedMethods
 /// @notice Interface for contracts that with non supported methods.
 interface INotSupportedMethods {
+    error ZETANotSupported();
     error CallOnRevertNotSupported();
 }
