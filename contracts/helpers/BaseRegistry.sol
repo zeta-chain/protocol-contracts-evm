9e01eaef71291bb5cb88a2c01469887d9ca907da
Denis Fadeev <denis@fadeev.org>
2025-10-20 15:48:55 +0300

    forge fmt

--- contracts/helpers/BaseRegistry.sol
+++ contracts/helpers/BaseRegistry.sol
@@ -101,7 +101,14 @@ abstract contract BaseRegistry is
     /// @param gasZRC20 The address of the ZRC20 token that represents gas token for the chain.
     /// @param registry Address of the Registry contract on the connected chain.
     /// @param activation Whether activate or deactivate the chain
-    function _changeChainStatus(uint256 chainId, address gasZRC20, bytes calldata registry, bool activation) internal {
+    function _changeChainStatus(
+        uint256 chainId,
+        address gasZRC20,
+        bytes calldata registry,
+        bool activation
+    )
+        internal
+    {
         // In the case chain is already activated
         if (_chains[chainId].active && activation) revert ChainActive(chainId);
         // In the case chain is inactive
@@ -140,7 +147,13 @@ abstract contract BaseRegistry is
     /// @param chainId The ID of the chain where the contract is deployed.
     /// @param contractType The type of the contract (e.g., "connector", "gateway").
     /// @param addressBytes The bytes representation of the non-EVM address.
-    function _registerContract(uint256 chainId, string calldata contractType, bytes calldata addressBytes) internal {
+    function _registerContract(
+        uint256 chainId,
+        string calldata contractType,
+        bytes calldata addressBytes
+    )
+        internal
+    {
         // Validate inputs
         if (!_chains[chainId].active) revert ChainNonActive(chainId);
         if (bytes(contractType).length == 0) revert InvalidContractType(contractType);
