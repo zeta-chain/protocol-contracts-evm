a994ea0aa4aa5689481f82af318983dbd427dcbf
s2imonovic <s2imonovic@gmail.com>
2025-10-20 15:40:43 +0200

    fix: remove ZETA onCall() from UniversalContract

--- contracts/zevm/interfaces/UniversalContract.sol
+++ contracts/zevm/interfaces/UniversalContract.sol
@@ -69,9 +69,6 @@ abstract contract UniversalContract {
         gateway = IGatewayZEVM(registry.gatewayZEVM());
     }
 
-    /// @notice Function to handle cross-chain calls with native ZETA transfers
-    function onCall(MessageContext calldata context, bytes calldata message) external payable virtual;
-
     /// @notice Function to handle cross-chain calls with ZRC20 token transfers
     function onCall(
         MessageContext calldata context,
