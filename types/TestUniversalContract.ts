bd96420e02f56e1a7bba152c282649ecda0c1a2d
s2imonovic <s2imonovic@gmail.com>
2025-10-22 16:19:49 +0200

    fix: generate

--- types/TestUniversalContract.ts
+++ types/TestUniversalContract.ts
@@ -225,7 +225,7 @@ export interface TestUniversalContract extends BaseContract {
   onRevert: TypedContractMethod<
     [revertContext: RevertContextStruct],
     [void],
-    "nonpayable"
+    "payable"
   >;
 
   registry: TypedContractMethod<[], [string], "view">;
@@ -261,7 +261,7 @@ export interface TestUniversalContract extends BaseContract {
   ): TypedContractMethod<
     [revertContext: RevertContextStruct],
     [void],
-    "nonpayable"
+    "payable"
   >;
   getFunction(
     nameOrSignature: "registry"
