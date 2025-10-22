1860a1991b57f9b54d77d492894fd46ad8815c5b
s2imonovic <s2imonovic@gmail.com>
2025-10-22 14:07:17 +0200

    fix: fmt

--- contracts/zevm/interfaces/UniversalContract.sol
+++ contracts/zevm/interfaces/UniversalContract.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-import { RevertOptions, RevertContext, AbortContext } from "../../../contracts/Revert.sol";
+import { AbortContext, RevertContext, RevertOptions } from "../../../contracts/Revert.sol";
 import "../interfaces/IGatewayZEVM.sol";
-import "../interfaces/IZRC20.sol";
 import "../interfaces/IWZETA.sol";
+import "../interfaces/IZRC20.sol";
 import "./ICoreRegistry.sol";
 
 /// @custom:deprecated should be removed once v2 SystemContract is not used anymore.
